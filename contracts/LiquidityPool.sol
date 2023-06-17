11ef719b94cac5cb3bbce2ac21c3bf6cc45c58fc
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-06-17 18:41:55 +0530

    updated pool contract

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -21,13 +21,20 @@ contract LiquidityPool {
         uint minBalance1;
         uint minBalance2;
     }
-    uint poolCount = 0;
+    struct Provider {
+        uint currentBalance1;
+        uint currentBalance2;
+        uint claimedBalance1;
+        uint claimedBalance2;
+    }
+
+    uint public poolCount = 0;
+    mapping(uint => mapping(address => Provider)) public providerDetails;
     mapping(uint => Pool) public pool;
-    mapping(IERC20 => uint[]) public tokensInPool;
-    mapping(uint => mapping(address => uint)) public providersLPT;
     mapping(uint => address[]) liquidityProviders;
     mapping(ERC20Token => mapping(ERC20Token => bool)) private tokenPairExists;
     mapping(ERC20Token => bool) private LPTokenExists;
+
     event poolCreated(address _poolCreator, string _tokenPair, uint timestamp);
     event liquidityAdded(
         address _liquidityProvider,
@@ -39,6 +46,8 @@ contract LiquidityPool {
     event liquidityRemoved(
         address _liquidityProvider,
         string _tokenPair,
+        uint _outAmount1,
+        uint _outAmount2,
         uint timestamp
     );
     event swappedTokens(
@@ -78,16 +87,21 @@ contract LiquidityPool {
         newPool.token2.transferFrom(msg.sender, address(this), _balance2);
         liquidityProviders[poolCount].push(msg.sender);
         newPool.LPT.transfer(msg.sender, newPool.LPTsupply);
-        providersLPT[poolCount][msg.sender] = newPool.LPTsupply;
-        tokensInPool[_token1].push(poolCount);
-        tokensInPool[_token2].push(poolCount);
+        Provider storage provider = providerDetails[poolCount][msg.sender];
+        provider.currentBalance1 = provider.currentBalance1.add(_balance1);
+        provider.currentBalance2 = provider.currentBalance2.add(_balance2);
+        providerDetails[poolCount][msg.sender] = provider;
         tokenPairExists[_token1][_token2] = true;
         tokenPairExists[_token2][_token1] = true;
         LPTokenExists[_LPT] = true;
         poolCount = poolCount + 1;
         emit poolCreated(
             owner,
-            string.concat(newPool.token1.symbol(), newPool.token2.symbol()),
+            string.concat(
+                newPool.token1.symbol(),
+                "/",
+                newPool.token2.symbol()
+            ),
             block.timestamp
         );
     }
@@ -100,23 +114,16 @@ contract LiquidityPool {
         uint reserve1 = pool[_id].balance1;
         uint reserve2 = pool[_id].balance2;
         if (_token == pool[_id].token1) {
-            return
-                _tokenAmount = (
-                    _amount.mul(reserve2).mul(10 ** 18).div(reserve1)
-                );
+            return _tokenAmount = (_amount.mul(reserve2).div(reserve1));
         } else {
-            return
-                _tokenAmount = (
-                    _amount.mul(reserve1).mul(10 ** 18).div(reserve2)
-                );
+            return _tokenAmount = (_amount.mul(reserve1).div(reserve2));
         }
     }
 
     function addLiquidity(uint _id, uint _amount1, uint _amount2) external {
-        uint userToken1Balance = pool[_id].token1.balanceOf(msg.sender);
-        uint userToken2Balance = pool[_id].token2.balanceOf(msg.sender);
         require(
-            userToken1Balance >= _amount1 && userToken2Balance >= _amount2,
+            pool[_id].token1.balanceOf(msg.sender) >= _amount1 &&
+                pool[_id].token2.balanceOf(msg.sender) >= _amount2,
             "Insufficient Balance"
         );
         uint reserve1 = pool[_id].balance1;
@@ -129,23 +136,25 @@ contract LiquidityPool {
         uint token2LPT = _amount2.mul(LPTAmount).div(reserve2);
         pool[_id].token1.transferFrom(msg.sender, address(this), _amount1);
         pool[_id].token2.transferFrom(msg.sender, address(this), _amount2);
+        Provider storage provider = providerDetails[_id][msg.sender];
+        provider.currentBalance1 = provider.currentBalance1.add(_amount1);
+        provider.currentBalance2 = provider.currentBalance2.add(_amount2);
+        providerDetails[_id][msg.sender] = provider;
         if (token1LPT < token2LPT) {
             lpToken.mint(msg.sender, token1LPT);
             pool[_id].LPTsupply = LPTAmount.add(token1LPT);
-            providersLPT[_id][msg.sender] = providersLPT[_id][msg.sender].add(
-                token1LPT
-            );
         } else {
             lpToken.mint(msg.sender, token2LPT);
             pool[_id].LPTsupply = LPTAmount.add(token2LPT);
-            providersLPT[_id][msg.sender] = providersLPT[_id][msg.sender].add(
-                token2LPT
-            );
         }
         liquidityProviders[_id].push(msg.sender);
         emit liquidityAdded(
             msg.sender,
-            string.concat(pool[_id].token1.symbol(), pool[_id].token2.symbol()),
+            string.concat(
+                pool[_id].token1.symbol(),
+                "/",
+                pool[_id].token2.symbol()
+            ),
             _amount1,
             _amount2,
             block.timestamp
@@ -153,14 +162,15 @@ contract LiquidityPool {
     }
 
     function removeLiquidity(uint _id, uint _LPTAmount) external {
+        ERC20Token lpToken = pool[_id].LPT;
         require(
-            _LPTAmount <= providersLPT[_id][msg.sender],
+            _LPTAmount <= lpToken.balanceOf((msg.sender)),
             "Insufficient LPT"
         );
         uint reserve1 = pool[_id].balance1;
         uint reserve2 = pool[_id].balance2;
         uint LPTsupply = pool[_id].LPTsupply;
-        ERC20Token lpToken = pool[_id].LPT;
+
         uint withdrawableToken1 = _LPTAmount.mul(reserve1).div(LPTsupply);
         uint withdrawableToken2 = _LPTAmount.mul(reserve2).div(LPTsupply);
         lpToken.burn(msg.sender, _LPTAmount);
@@ -168,13 +178,30 @@ contract LiquidityPool {
         pool[_id].balance2 = reserve2.sub(withdrawableToken2);
         pool[_id].token1.transfer(msg.sender, withdrawableToken1);
         pool[_id].token2.transfer(msg.sender, withdrawableToken2);
-        pool[_id].LPTsupply = LPTsupply.sub(_LPTAmount);
-        providersLPT[_id][msg.sender] = providersLPT[_id][msg.sender].sub(
-            _LPTAmount
+        Provider storage provider = providerDetails[_id][msg.sender];
+        provider.currentBalance1 = provider.currentBalance1.sub(
+            withdrawableToken1
         );
+        provider.currentBalance2 = provider.currentBalance2.sub(
+            withdrawableToken2
+        );
+        provider.claimedBalance1 = provider.claimedBalance1.add(
+            withdrawableToken1
+        );
+        provider.claimedBalance2 = provider.claimedBalance2.add(
+            withdrawableToken2
+        );
+        providerDetails[_id][msg.sender] = provider;
+        pool[_id].LPTsupply = LPTsupply.sub(_LPTAmount);
         emit liquidityRemoved(
             msg.sender,
-            string.concat(pool[_id].token1.symbol(), pool[_id].token2.symbol()),
+            string.concat(
+                pool[_id].token1.symbol(),
+                "/",
+                pool[_id].token2.symbol()
+            ),
+            withdrawableToken1,
+            withdrawableToken2,
             block.timestamp
         );
     }
@@ -185,9 +212,6 @@ contract LiquidityPool {
             ERC20Token lpToken = pool[_id].LPT;
             lpToken.mint(provider, _feePerProvider);
             pool[_id].LPTsupply = pool[_id].LPTsupply.add(_feePerProvider);
-            providersLPT[_id][provider] = providersLPT[_id][provider].add(
-                _feePerProvider
-            );
         }
     }
 
@@ -230,7 +254,11 @@ contract LiquidityPool {
         }
         emit swappedTokens(
             msg.sender,
-            string.concat(pool[_id].token1.symbol(), pool[_id].token2.symbol()),
+            string.concat(
+                pool[_id].token1.symbol(),
+                "/",
+                pool[_id].token2.symbol()
+            ),
             _amount,
             outAmount,
             block.timestamp
