122c2ab30f96ee5c4246f4b7233eff87ac4b50e4
Lakshmishreya Bapatuyeruguti <lakshmishreya@simformsolutions.com>
2023-06-20 11:09:25 +0530

    Updated test cases

--- test/LiquidityPool.js
+++ test/LiquidityPool.js
@@ -18,50 +18,64 @@ describe('Liquidity Pool Testing', function () {
   describe('checking if is pool owner matches with deployer', function () {
     it('Should be owner Only', async () => {
       const [owner] = await ethers.getSigners();
-      const { poolInstance } = await loadFixture(deployFixture);
+      console.log('HIIIIIIIIIIIIIIIIIIIIIIIII');
+      const { poolInstance } = await deployFixture();
       let poolOwner = owner.address;
       expect(await poolInstance.owner()).to.equal(poolOwner);
     });
   });
   describe('checking if the pool is created by the owner', function () {
     it('User cannot create pool', async () => {
-      const { poolInstance, swapUser, liquidityProvider } = await loadFixture(
-        deployFixture,
-      );
-      const token1 = '0x6a1149ea9Cc1812C1282D653D0103AdF9F77eD6d';
-      const token2 = '0xeB82Ae2ffbc65A8dbF718C74f7d111a2E2bF193c';
-      const lpt = '0xA409D4670D3dfdf78C28BEF41b290c9952B18CE0';
+      const { poolInstance, swapUser, liquidityProvider } =
+        await deployFixture();
+      const token1 = await ethers.deployContract('ERC20TestToken');
+      const token2 = await ethers.deployContract('ERC20TestToken');
+      const lpt = await ethers.deployContract('ERC20Token', [
+        poolInstance.target,
+      ]);
+      token1.approve(poolInstance.target, 100);
+      token2.approve(poolInstance.target, 200);
       await expect(
         poolInstance
           .connect(swapUser)
-          .createPool(token1, token2, 100, 100, lpt),
+          .createPool(token1.target, token2.target, 100, 200, lpt.target),
       ).to.be.revertedWith('Only Owner Allowed');
     });
     it('provider cannot create pool', async () => {
-      const { poolInstance, liquidityProvider } = await loadFixture(
-        deployFixture,
-      );
-      const token1 = '0x6a1149ea9Cc1812C1282D653D0103AdF9F77eD6d';
-      const token2 = '0xeB82Ae2ffbc65A8dbF718C74f7d111a2E2bF193c';
-      const lpt = '0xA409D4670D3dfdf78C28BEF41b290c9952B18CE0';
+      const { poolInstance, liquidityProvider } = await deployFixture();
+
+      const token1 = await ethers.deployContract('ERC20TestToken');
+      const token2 = await ethers.deployContract('ERC20TestToken');
+      const lpt = await ethers.deployContract('ERC20Token', [
+        poolInstance.target,
+      ]);
+      token1.approve(poolInstance.target, 100);
+      token2.approve(poolInstance.target, 200);
       await expect(
         poolInstance
           .connect(liquidityProvider)
-          .createPool(token1, token2, 100, 100, lpt),
+          .createPool(token1.target, token2.target, 100, 200, lpt.target),
       ).to.be.revertedWith('Only Owner Allowed');
     });
-    it('Pool Owner Able create pool', async () => {
-      const { poolInstance, liquidityProvider } = await loadFixture(
-        deployFixture,
-      );
-      const token1 = '0x6a1149ea9Cc1812C1282D653D0103AdF9F77eD6d';
-      const token2 = '0xeB82Ae2ffbc65A8dbF718C74f7d111a2E2bF193c';
-      const lpt = '0xA409D4670D3dfdf78C28BEF41b290c9952B18CE0';
+    it('owner creating pool', async () => {
+      const { poolInstance, liquidityProvider } = await deployFixture();
+
+      const token1 = await ethers.deployContract('ERC20TestToken');
+      const token2 = await ethers.deployContract('ERC20TestToken');
+      const lpt = await ethers.deployContract('ERC20Token', [
+        poolInstance.target,
+      ]);
+      token1.approve(poolInstance.target, 100);
+      token2.approve(poolInstance.target, 200);
       await expect(
-        poolInstance
-          .connect(liquidityProvider)
-          .createPool(token1, token2, 100, 100, lpt),
-      ).to.be.revertedWith('Only Owner Allowed');
+        poolInstance.createPool(
+          token1.target,
+          token2.target,
+          100,
+          200,
+          lpt.target,
+        ),
+      ).to.not.be.reverted;
     });
   });
 });
