90948ad8855cc843e4227c3bbfdbf1247a3655c8
Manav <manavpanchal@simformsolutions.com>
2023-06-23 16:02:32 +0530

    changed old contract details to new one

--- web-app/src/utils/constants.js
+++ web-app/src/utils/constants.js
@@ -1,442 +1,442 @@
 import { ethLogo } from '../images/images';
 
 export const LiquidityPoolABI = [
-  {
-    inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: '_liquidityProvider',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'string',
-        name: '_tokenPair',
-        type: 'string',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: '_inAmount1',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: '_inAmount2',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'timestamp',
-        type: 'uint256',
-      },
-    ],
-    name: 'liquidityAdded',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: '_liquidityProvider',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'string',
-        name: '_tokenPair',
-        type: 'string',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: '_outAmount1',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: '_outAmount2',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'timestamp',
-        type: 'uint256',
-      },
-    ],
-    name: 'liquidityRemoved',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: '_poolCreator',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'string',
-        name: '_tokenPair',
-        type: 'string',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'timestamp',
-        type: 'uint256',
-      },
-    ],
-    name: 'poolCreated',
-    type: 'event',
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: 'address',
-        name: '_user',
-        type: 'address',
-      },
-      {
-        indexed: false,
-        internalType: 'string',
-        name: '_tokenPair',
-        type: 'string',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: '_inAmount',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: '_outAmount',
-        type: 'uint256',
-      },
-      {
-        indexed: false,
-        internalType: 'uint256',
-        name: 'timestamp',
-        type: 'uint256',
-      },
-    ],
-    name: 'swappedTokens',
-    type: 'event',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_id',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token1',
-        type: 'address',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token2',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: '_amount1',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: '_amount2',
-        type: 'uint256',
-      },
-    ],
-    name: 'addLiquidity',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_id',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: '_amount',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token',
-        type: 'address',
-      },
-    ],
-    name: 'calculateSwappingAmount',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '_tokenAmount',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token2',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_id',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: '_amount',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token',
-        type: 'address',
-      },
-    ],
-    name: 'calculateTokenAmount',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '_tokenAmount',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'contract IERC20',
-        name: '_token1',
-        type: 'address',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token2',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: '_balance1',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: '_balance2',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract ERC20Token',
-        name: '_LPT',
-        type: 'address',
-      },
-    ],
-    name: 'createPool',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'owner',
-    outputs: [
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    name: 'pool',
-    outputs: [
-      {
-        internalType: 'contract IERC20',
-        name: 'token1',
-        type: 'address',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: 'token2',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'balance1',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'balance2',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract ERC20Token',
-        name: 'LPT',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'LPTsupply',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'minBalance1',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'minBalance2',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [],
-    name: 'poolCount',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-      {
-        internalType: 'address',
-        name: '',
-        type: 'address',
-      },
-    ],
-    name: 'providerDetails',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: 'providedBalance1',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'providedBalance2',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'claimedBalance1',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'claimedBalance2',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'LPTCount',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_id',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: '_LPTAmount',
-        type: 'uint256',
-      },
-    ],
-    name: 'removeLiquidity',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: '_id',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: '_amount',
-        type: 'uint256',
-      },
-      {
-        internalType: 'contract IERC20',
-        name: '_token',
-        type: 'address',
-      },
-    ],
-    name: 'swapTokens',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function',
-  },
-];
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_liquidityProvider",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "_tokenPair",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_inAmount1",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_inAmount2",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "liquidityAdded",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_liquidityProvider",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "_tokenPair",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_outAmount1",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_outAmount2",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "liquidityRemoved",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_poolCreator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "_tokenPair",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "poolCreated",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "_tokenPair",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_inAmount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_outAmount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "swappedTokens",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token1",
+				"type": "address"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token2",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount2",
+				"type": "uint256"
+			}
+		],
+		"name": "addLiquidity",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"name": "calculateSwappingAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tokenAmount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token2",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"name": "calculateTokenAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tokenAmount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "_token1",
+				"type": "address"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token2",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_balance1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_balance2",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract ERC20Token",
+				"name": "_LPT",
+				"type": "address"
+			}
+		],
+		"name": "createPool",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "pool",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "token1",
+				"type": "address"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "token2",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "balance1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "balance2",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract ERC20Token",
+				"name": "LPT",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "LPTsupply",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "minBalance1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "minBalance2",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "poolCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "providerDetails",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "providedBalance1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "providedBalance2",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "claimedBalance1",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "claimedBalance2",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "LPTCount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_LPTAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "removeLiquidity",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "contract IERC20",
+				"name": "_token",
+				"type": "address"
+			}
+		],
+		"name": "swapTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
 export const LPTABI = [
   {
     inputs: [
@@ -948,42 +948,42 @@ export const pools = [
     id: 0,
     token1Address: '0x6a1149ea9Cc1812C1282D653D0103AdF9F77eD6d',
     token2Address: '0xeB82Ae2ffbc65A8dbF718C74f7d111a2E2bF193c',
-    LPTAddress: '0x7C2140866f0FC6cc6Fbc337abAF69Cf2966971C8',
+    LPTAddress: '0xF3FA7ce2B99aef2F0D13A2C9773d2a6a6417BA45',
     tokenPair: 'DAI-USDT',
   },
   {
     id: 1,
     token1Address: '0x6a1149ea9Cc1812C1282D653D0103AdF9F77eD6d',
     token2Address: '0x24e303D9c1A6032463524EFc070649f0AF7f016e',
-    LPTAddress: '0x583ed1580F89C9b3541d5Fa393e30360c911D981',
+    LPTAddress: '0x37380695F71B693a6464A1B6C9fdE70AF55D7484',
     tokenPair: 'DAI-MYT',
   },
   {
     id: 2,
     token1Address: '0x6a1149ea9Cc1812C1282D653D0103AdF9F77eD6d',
     token2Address: '0x5d9a020d55eF31Bc391c9629471e4B63A3A80aA8',
-    LPTAddress: '0xe559ADEC67b4130FEb019150cb70E570a4bB3556',
+    LPTAddress: '0x6527a532166C9f32C06e834017C9220A2897853b',
     tokenPair: 'DAI-DFI',
   },
   {
     id: 3,
     token1Address: '0xeB82Ae2ffbc65A8dbF718C74f7d111a2E2bF193c',
     token2Address: '0x24e303D9c1A6032463524EFc070649f0AF7f016e',
-    LPTAddress: '0xaaf380C61214c351C6357aeF1d40FD98ecafa29e',
+    LPTAddress: '0x2DFB5624Ef56da66EE12e5D62ed88c8C6E598C74',
     tokenPair: 'USDT-MYT',
   },
   {
     id: 4,
     token1Address: '0xeB82Ae2ffbc65A8dbF718C74f7d111a2E2bF193c',
     token2Address: '0x5d9a020d55eF31Bc391c9629471e4B63A3A80aA8',
-    LPTAddress: '0x7ABCcc1827651DE342EB204E7912b4c0530124B6',
+    LPTAddress: '0x2c989cE5951bf975ad62c8862419Ee5A0c7682F6',
     tokenPair: 'USDT-DFI',
   },
   {
     id: 5,
     token1Address: '0x24e303D9c1A6032463524EFc070649f0AF7f016e',
     token2Address: '0x5d9a020d55eF31Bc391c9629471e4B63A3A80aA8',
-    LPTAddress: '0x81bb2B6e13D48f758F0892AD227E7c9A5f096C6b',
+    LPTAddress: '0xa3E87080c8f02B6EDd3E0C30C8e83bc45587Dd56',
     tokenPair: 'MYT-DFI',
   },
 ];
