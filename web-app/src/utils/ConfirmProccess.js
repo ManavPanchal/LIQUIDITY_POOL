6225a04e25f56af73113afb59e3ef640dd448b4b
Manav <manavpanchal@simformsolutions.com>
2023-06-23 18:34:54 +0530

    handled some error and reverse pair scenario

--- web-app/src/utils/ConfirmProccess.js
+++ web-app/src/utils/ConfirmProccess.js
@@ -1,12 +1,13 @@
-import { pools } from './constants';
-import { ethers } from 'ethers';
-import lptInstance from './lptInstance';
-import poolInstance from './poolInstance';
-import SwappingInstance from './swappingInstance';
-import tokensInstance from './tokensInstance';
+import { pools, Tokens } from "./constants";
+import { ethers } from "ethers";
+import lptInstance from "./lptInstance";
+import poolInstance from "./poolInstance";
+import SwappingInstance from "./swappingInstance";
+import tokensInstance from "./tokensInstance";
+import { toast } from "react-toastify";
 
 const headers = {
-  'Content-Type': 'application/json',
+  "Content-Type": "application/json",
 };
 
 export const confirmProccess = async (
@@ -14,32 +15,32 @@ export const confirmProccess = async (
   from,
   setAllowanceWaiting,
   setTransactionFlag,
-  setConfirmTransactionToggle,
+  setConfirmTransactionToggle
 ) => {
   try {
-    const { contract: poolContract, networkId } = await poolInstance();
-    if (!(from === 'RemoveLiquidity')) {
+    const { contract: poolContract } = await poolInstance();
+    if (!(from === "RemoveLiquidity")) {
       let { contract: token1Contract, signerAddress } = await tokensInstance(
-        tokens.token1?.address,
+        tokens.token1?.address
       );
       let token1Allowance = await token1Contract.allowance(
         signerAddress,
-        process.env.REACT_APP_LIQUIDITY_CONTRACT,
+        process.env.REACT_APP_LIQUIDITY_CONTRACT
       );
       if (Number(token1Allowance) / 10 ** 18 < tokens?.token1Amount) {
         const tx = await token1Contract.approve(
           process.env.REACT_APP_LIQUIDITY_CONTRACT,
-          ethers.utils.parseEther(tokens?.token1Amount),
+          ethers.utils.parseEther(tokens?.token1Amount)
         );
         setAllowanceWaiting(true);
         await tx.wait();
         setAllowanceWaiting(false);
         token1Allowance = await token1Contract.allowance(
           signerAddress,
-          process.env.REACT_APP_LIQUIDITY_CONTRACT,
+          process.env.REACT_APP_LIQUIDITY_CONTRACT
         );
       }
-      if (from === 'AddLiquidity') {
+      if (from === "AddLiquidity") {
         const {
           contract: tokenContract,
           signerAddress,
@@ -48,19 +49,19 @@ export const confirmProccess = async (
         console.log();
         let token2Allowance = await tokenContract.allowance(
           signerAddress,
-          process.env.REACT_APP_LIQUIDITY_CONTRACT,
+          process.env.REACT_APP_LIQUIDITY_CONTRACT
         );
         if (Number(token2Allowance) / 10 ** 18 < tokens?.token2Amount) {
           const tx = await tokenContract.approve(
             process.env.REACT_APP_LIQUIDITY_CONTRACT,
-            ethers.utils.parseEther(tokens?.token2Amount),
+            ethers.utils.parseEther(tokens?.token2Amount)
           );
           setAllowanceWaiting(true);
           await tx.wait();
           setAllowanceWaiting(false);
           token2Allowance = await tokenContract.allowance(
             signerAddress,
-            process.env.REACT_APP_LIQUIDITY_CONTRACT,
+            process.env.REACT_APP_LIQUIDITY_CONTRACT
           );
         }
         if (
@@ -68,62 +69,46 @@ export const confirmProccess = async (
           Number(token2Allowance) / 10 ** 18 >= tokens?.token2Amount
         ) {
           setTransactionFlag(true);
-          console.log('...', tokens.pool?.poolId);
+          console.log("...", tokens.pool?.poolId);
           const tx = await poolContract.addLiquidity(
             tokens.pool?.poolId,
             tokens.token1?.address,
             tokens.token2?.address,
             ethers.utils.parseEther(tokens?.token1Amount),
-            ethers.utils.parseEther(tokens?.token2Amount),
+            ethers.utils.parseEther(tokens?.token2Amount)
           );
           await tx.wait();
-          const currentTokenPair = pools.filter(
-            (pool) => pool.id === tokens.pool?.poolId,
-          );
           const requestBody = {
             userAddress: signerAddress,
             poolId: tokens.pool?.poolId,
-            activity: 'Added',
-            tokenPair: currentTokenPair[0].tokenPair,
+            activity: "Added",
+            tokenPair: `${tokens.token1?.name}-${tokens.token2?.name}`,
             amount1: Number(tokens?.token1Amount),
             amount2: Number(tokens?.token2Amount),
             networkId,
           };
-          console.log(requestBody, '.............');
-          const response = await fetch('/api/addLiquidity', {
-            method: 'POST',
+          console.log(requestBody, ".............");
+          const response = await fetch("/api/addLiquidity", {
+            method: "POST",
             body: JSON.stringify(requestBody),
             headers,
           });
 
           if (response.status === 200) {
-            console.log('success');
+            console.log("success");
           }
           setConfirmTransactionToggle(false);
         } else {
-          alert('please give sufficient Allowance');
+          alert("please give sufficient Allowance");
           confirmProccess();
         }
       } else {
         if (Number(token1Allowance) / 10 ** 18 >= tokens?.token1Amount) {
           setTransactionFlag(true);
           await SwappingInstance(tokens);
-          await fetch('/api/swapTokens', {
-            method: 'POST',
-            body: JSON.stringify({
-              userAddress: signerAddress,
-              poolId: tokens.pool?.poolId,
-              activity: 'Swapped',
-              tokenPair: pools[tokens.pool?.poolId].tokenPair,
-              amount1: tokens.token1Amount,
-              amount2: tokens.token2Amount,
-              networkId,
-            }),
-            headers,
-          });
           setConfirmTransactionToggle(false);
         } else {
-          alert('please give sufficient Allowance');
+          alert("please give sufficient Allowance");
           confirmProccess();
         }
       }
@@ -135,19 +120,19 @@ export const confirmProccess = async (
       } = await lptInstance(pools[tokens.pool?.poolId].LPTAddress);
       let LPTAllowance = await LPTContract.allowance(
         signerAddress,
-        process.env.REACT_APP_LIQUIDITY_CONTRACT,
+        process.env.REACT_APP_LIQUIDITY_CONTRACT
       );
       if (Number(LPTAllowance) / 10 ** 18 < tokens?.LPTAmount) {
         const tx = await LPTContract.approve(
           process.env.REACT_APP_LIQUIDITY_CONTRACT,
-          ethers.utils.parseEther(tokens?.LPTAmount),
+          ethers.utils.parseEther(tokens?.LPTAmount)
         );
         setAllowanceWaiting(true);
         await tx.wait();
         setAllowanceWaiting(false);
         LPTAllowance = await LPTContract.allowance(
           signerAddress,
-          process.env.REACT_APP_LIQUIDITY_CONTRACT,
+          process.env.REACT_APP_LIQUIDITY_CONTRACT
         );
       }
       if (Number(LPTAllowance) / 10 ** 18 >= tokens?.LPTAmount) {
@@ -155,59 +140,77 @@ export const confirmProccess = async (
         async function checkClaimedAmount() {
           const providerDetails = await poolContract.providerDetails(
             tokens.pool?.poolId,
-            signerAddress,
+            signerAddress
           );
           const currentClaimed1 = ethers.utils.formatUnits(
-            providerDetails.claimedBalance1,
+            providerDetails.claimedBalance1
           );
           const currentClaimed2 = ethers.utils.formatUnits(
-            providerDetails.claimedBalance2,
+            providerDetails.claimedBalance2
           );
-          console.log(currentClaimed1, '....1', currentClaimed2, '.....2');
+          console.log(currentClaimed1, "....1", currentClaimed2, ".....2");
           return { currentClaimed1, currentClaimed2 };
         }
         const { currentClaimed1, currentClaimed2 } = await checkClaimedAmount();
 
         const tx = await poolContract.removeLiquidity(
           tokens.pool?.poolId,
-          ethers.utils.parseEther(tokens?.LPTAmount),
+          ethers.utils.parseEther(tokens?.LPTAmount)
         );
         await tx.wait();
         const {
           currentClaimed1: updatedClaimed1,
           currentClaimed2: updatedClaimed2,
         } = await checkClaimedAmount();
-        console.log(updatedClaimed1, '....1', updatedClaimed2, '.....2');
+        console.log(updatedClaimed1, "....1", updatedClaimed2, ".....2");
         const currentTokenPair = pools.filter(
-          (pool) => pool.id === tokens.pool?.poolId,
+          (pool) => pool.id === tokens.pool?.poolId
         );
         const requestBody = {
           userAddress: signerAddress,
           poolId: tokens.pool?.poolId,
-          activity: 'Removed',
+          activity: "Removed",
           tokenPair: currentTokenPair[0].tokenPair,
           amount1: Number((updatedClaimed1 - currentClaimed1).toFixed(18)),
           amount2: Number((updatedClaimed2 - currentClaimed2).toFixed(18)),
           networkId,
         };
-        console.log(requestBody, '.............');
-        const response = await fetch('/api/removeLiquidity', {
-          method: 'POST',
+        console.log(requestBody, ".............");
+        const response = await fetch("/api/removeLiquidity", {
+          method: "POST",
           headers,
           body: JSON.stringify(requestBody),
         });
         if (response.status === 200) {
-          console.log('success');
+          console.log("success");
         }
         setConfirmTransactionToggle(false);
       } else {
-        alert('please give sufficient Allowance');
+        alert("please give sufficient Allowance");
         confirmProccess();
       }
     }
   } catch (error) {
-    if (!error?.message.includes('user rejected transaction'))
-      console.log('inner' + error);
+    const errorMessage =
+      'cannot estimate gas; transaction may fail or may require manual gas limit [ See: https://links.ethers.org/v5-errors-UNPREDICTABLE_GAS_LIMIT ] (reason="execution reverted: Invalid Ratio", method="estimateGas", transaction={...})';
+    const regex = /reverted: (.*?)\",/;
+    const match = errorMessage.match(regex);
+
+    if (match && match.length > 1) {
+      const revertedMessage = match[1];
+      toast.error(revertedMessage,{
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        })
+    } else {
+      console.log(error);
+    }
     setConfirmTransactionToggle(false);
   }
 };
--- web-app/src/utils/swappingInstance.js
+++ web-app/src/utils/swappingInstance.js
@@ -1,15 +1,29 @@
 import { ethers } from "ethers";
 import walletInstance from "./walletInstance";
-import { LiquidityPoolABI, tokenABI } from "./constants";
+import { LiquidityPoolABI, pools, tokenABI } from "./constants";
 import { toast } from "react-toastify";
 
 async function SwappingInstance(tokens) {
   try {
-    const { signer } = await walletInstance();
+    const { signer, signerAddress, networkId } = await walletInstance();
     const contract  = new ethers.Contract(process.env.REACT_APP_LIQUIDITY_CONTRACT, LiquidityPoolABI, signer);
     const tx = await contract.swapTokens(tokens.pool?.poolId,`${(tokens.token1Amount * (10**18))}`, tokens.token1?.address)
     await tx.wait();
-
+    await fetch('/api/swapTokens', {
+      method: 'POST',
+      body: JSON.stringify({
+        userAddress: signerAddress,
+        poolId: tokens.pool?.poolId,
+        activity: 'Swapped',
+        tokenPair: `${tokens.token1?.name}-${tokens.token2?.name}`,
+        amount1: tokens.token1Amount,
+        amount2: tokens.token2Amount,
+        networkId,
+      }),
+      headers : {
+        'Content-Type': 'application/json',
+      }
+    })
   } catch (error) {
     if(error.toString().includes("reverted: Exceeds Min Balance"))
     toast.info('Sorry! Swapping is not possible with entered amount you can try again on less amount', {
