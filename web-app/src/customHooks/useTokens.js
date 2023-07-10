3bbaa4e346df83c1af84e17787952bdb030346c1
Manav <manavpanchal@simformsolutions.com>
2023-07-10 17:59:11 +0530

    small changes in custome hook

--- web-app/src/customHooks/useTokens.js
+++ web-app/src/customHooks/useTokens.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { useCallback, useEffect, useMemo, useState } from "react"
 
 const useTokens = (initialToken) =>{
     const [tokens, setTokens] =  useState(initialToken);
@@ -7,29 +7,29 @@ const useTokens = (initialToken) =>{
 
     const token1 = {
         name:useMemo(()=> tokens.token1?.name,[tokens.token1?.name]),
-        address: useMemo(()=> tokens.token1?.address,[tokens.token1?.address]),
-        balance: useMemo(()=> tokens.token1?.balance,[tokens.token1?.balance]),
-        amount: useMemo(()=> tokens.token1?.amount,[tokens.token1?.amount]),
-        logo:useMemo(()=> tokens.token1?.logo,[tokens.token1?.logo]),
+        address: useMemo(() => tokens.token1?.address,[tokens.token1?.address]),
+        balance: useMemo(() => tokens.token1?.balance,[tokens.token1?.balance]),
+        amount: useMemo(() => tokens.token1?.amount,[tokens.token1?.amount]),
+        logo:useMemo(() => tokens.token1?.logo,[tokens.token1?.logo]),
         isSelected: useMemo(()=> tokens.token1?.isSelected,[tokens.token1?.isSelected]),
-        select : () =>{
+        select : useCallback(() =>{
             setTokens( prevVal => { return {token1:{...prevVal.token1, isSelected:true}, token2:{...prevVal.token2, isSelected:false}}})
-        },
-        setName: (name)=>{
+        },[]),
+        setName: useCallback((name)=>{
             setTokens( prevVal => { return {...prevVal,token1:{...prevVal.token1, name}}})
-        },
-        setAddress: (address)=>{
+        },[]),
+        setAddress: useCallback((address)=>{
             setTokens( prevVal => { return {...prevVal,token1:{...prevVal.token1, address}}})
-        },
-        setBalance : (balance)=>{
+        },[]),
+        setBalance : useCallback((balance)=>{
             setTokens( prevVal => { return {...prevVal,token1:{...prevVal.token1, balance}}})
-        },
-        setLogo: (logo) =>{
+        },[]),
+        setLogo: useCallback((logo) =>{
             setTokens( prevVal => { return {...prevVal,token1:{...prevVal.token1, logo}}})
-        },
-        setAmount: (amount) =>{
+        },[]),
+        setAmount: useCallback((amount) =>{
             setTokens( prevVal => { return {...prevVal,token1:{...prevVal.token1, amount}}})
-        }
+        },[])
     }
 
     const token2 = {
@@ -39,24 +39,24 @@ const useTokens = (initialToken) =>{
         amount: useMemo(()=> tokens.token2?.amount,[tokens.token2?.amount]),
         logo:useMemo(()=> tokens.token2?.logo,[tokens.token2?.logo]),
         isSelected: useMemo(()=> tokens.token2?.isSelected,[tokens.token2?.isSelected]),
-        select : () =>{
+        select : useCallback(() =>{
             setTokens( prevVal => { return {token2:{...prevVal.token2, isSelected:true}, token1:{...prevVal.token1, isSelected:false}}})
-        },
-        setName: (name)=>{
+        },[]),
+        setName: useCallback((name)=>{
             setTokens( prevVal => { return {...prevVal,token2:{...prevVal.token2, name}}})
-        },
-        setAddress: (address)=>{
+        },[]),
+        setAddress: useCallback((address)=>{
             setTokens( prevVal => { return {...prevVal,token2:{...prevVal.token2, address}}})
-        },
-        setBalance : (balance)=>{
+        },[]),
+        setBalance : useCallback((balance)=>{
             setTokens( prevVal => { return {...prevVal,token2:{...prevVal.token2, balance}}})
-        },
-        setLogo:(logo)=>{
+        },[]),
+        setLogo: useCallback((logo)=>{
             setTokens( prevVal => { return {...prevVal,token2:{...prevVal.token2, logo}}})
-        },
-        setAmount: (amount) =>{
+        },[]),
+        setAmount: useCallback((amount) =>{
             setTokens( prevVal => { return {...prevVal,token2:{...prevVal.token2, amount}}})
-        }
+        },[])
     }
 
     const pool = useMemo(()=>tokens.pool,[tokens.pool])
